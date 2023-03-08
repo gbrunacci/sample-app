e11200ac702af26ddf4cbb617f010fabcd8b5285
Gabriel Brunacci <gbrunacci@gmail.com>
2023-03-08 16:51:45 +0100

    hotfix: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
       <h1>This is my fifth commit</h1>
       <h1>This is my sixth commit</h1>
       <h1>This is my 8 commit</h1>
+      <h1>This is my HOTFIX commit</h1>
     </main>
   )
 }
