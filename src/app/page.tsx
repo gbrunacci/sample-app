65cee4dac2f6244242c91cf869c17264397f3777
Gabriel Brunacci <gbrunacci@gmail.com>
2023-03-08 16:06:21 +0100

    chore: third

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main>
       <h1>This is my first commit</h1>
       <h1>This is my second commit</h1>
+      <h1>This is my third commit</h1>
     </main>
   )
 }
