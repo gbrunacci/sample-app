cf42b56a246e04776a7e0b32c5f2be699905caa2
Gabriel Brunacci <gbrunacci@gmail.com>
2023-03-30 11:02:16 +0200

    chore: attempt

--- .automation/pscale/check-branch-exists.sh
+++ .automation/pscale/check-branch-exists.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 . ./lib/use-pscale-docker-image.sh
-. ./lib/wait-for-branch-readiness.sh
-
 . ./lib/authenticate-ps.sh
 
 BRANCH_NAME="$1"
 
 echo "Checking if branch $BRANCH_NAME exists and is ready"
 
-RAW_OUTPUT=$(pscale branch list "$DB_NAME" --org "$ORG_NAME" --format json)
+RAW_OUTPUT=$(pscale branch list $DB_NAME --org $ORG_NAME --format json)
 
-# check return code, if not 0 then error
 if [ $? -ne 0 ]; then
     echo "Error: pscale branch list returned non-zero exit code $?: $RAW_OUTPUT"
     exit 1
